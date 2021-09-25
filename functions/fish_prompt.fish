68a1e14318e43a994466518c933f21220e8ef24d
Yū Kobayashi <kb10uy@kb10uy.org>
2021-09-25 16:18:38 +0900

    Update fish_prompt.fish

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
   set -l stat $status
-  set -q ract_host_fg || set ract_host_fg 3D701A
+  set -q ract_host_fg || set ract_host_fg FCAAF5
   set -q ract_host_bg || set ract_host_bg 284D91
 
   # User and host
